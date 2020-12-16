26b24ffcf1c9fc78d8ca1217e32d5c625c05f874
mahmoodnael <m7mood.nael.1996@gmail.com>
2020-12-16 13:34:57 +0200

    add sql files to init database, add api for projects and employees, add script for tests and mock data

---
+++ db_api/employee_api.py
@@ -0,0 +1,68 @@
+from config import connection
+
+def get_constractor():
+
+    with connection.cursor() as cursor:
+        query = """ SELECT * FROM constractor """
+        cursor.execute(query, ())
+        connection.commit()
+        result = cursor.fetchone()
+    return result
+
+def create_constractor(constractor_id,name):
+    with connection.cursor() as cursor:
+        query=""" INSERT INTO constractor (id,name) VALUES(%s,%s)"""
+        cursor.execute(query,(constractor_id, name ))
+        connection.commit()
+
+def get_employee_by_id(employee_id):
+    with connection.cursor() as cursor:
+        query = """ SELECT * FROM employee WHERE id = %s """
+        cursor.execute(query, (employee_id,))
+        connection.commit()
+        result = cursor.fetchone()
+    if result == None:
+        raise Exception("Error, this employee does not exist")
+    return result
+
+def get_all_employees():
+    with connection.cursor() as cursor:
+        query = """ SELECT * FROM employee """
+        cursor.execute(query, ())
+        connection.commit()
+        result = cursor.fetchall()
+    return result
+
+def add_new_employee(employee_id, employee_name, employee_phone_number, employee_job):
+    constractor_id = get_constractor()['id']
+    with connection.cursor() as cursor:
+        query=""" INSERT INTO employee (id,name,phone_number,job,constructor_id) VALUES(%s,%s,%s,%s,%s)"""
+        cursor.execute(query,(employee_id,employee_name,employee_phone_number,employee_job,constractor_id ))
+        connection.commit()
+
+
+def delete_employee(employee_id):
+    with connection.cursor() as cursor:
+        query = """ DELETE FROM employee WHERE id = %s """
+        cursor.execute(query, (employee_id, ))
+        connection.commit()
+
+def free_employee_status(employee_id):
+    with connection.cursor() as cursor:
+        query=""" UPDATE employee set status=%s , project_id=%s WHERE id=%s """
+        cursor.execute(query,('true',None,employee_id, ))
+        connection.commit()
+
+def set_employee_status(employee_id, project_id):
+    with connection.cursor() as cursor:
+        query=""" UPDATE employee set status=%s , project_id=%s WHERE id=%s """
+        cursor.execute(query,('false',project_id,employee_id, ))
+        connection.commit()
+
+def get_all_employees_works_in_project(project_id):
+    with connection.cursor() as cursor:
+        query = """ SELECT * FROM employee WHERE project_id=%s """
+        cursor.execute(query, (project_id))
+        connection.commit()
+        result = cursor.fetchall()
+    return result
---
+++ tests.py
@@ -0,0 +1,32 @@
+from db_api import employee_api, projects_api
+
+
+constractor = {"id": "1", "name":"Mahmood Qawasmi"}
+
+employees = [{"id" : "12345", "name": "Mohamma Abid", "phone": "0500000000", "job" : "graphic designer"}, {"id" : "12346", "name": "Mohamma Jayousi", "phone": "0544444444", "job" : "backend"}, {"id" : "123", "name": "fathy alnasab", "phone": "0533333333", "job" : "nasab"}]
+
+#employee_api.create_constractor(constractor["id"],constractor["name"])
+# for employee in employees:
+    # employee_api.add_new_employee(employee["id"],employee["name"],employee["phone"],employee["job"])
+
+print(employee_api.get_constractor())
+print("\n")
+print(employee_api.get_all_employees())
+print("\n")
+#print(employee_api.get_employee_by_id(12345)) if it doesnot excist
+print("\n")
+#employee_api.delete_employee(12345)
+#print(employee_api.get_all_employees())
+print("\n")
+employee_api.free_employee_status(123)
+print(employee_api.get_all_employees())
+
+projects_api.add_new_project("microsoft building","Jerusalem",1,"build a new high tech building for microsoft")
+
+print(projects_api.get_all_projects())
+print("\n")
+print(projects_api.get_project_by_id(1))
+employee_api.set_employee_status(123,1)
+print(employee_api.get_all_employees())
+
+print(employee_api.get_all_employees_works_in_project(1))
