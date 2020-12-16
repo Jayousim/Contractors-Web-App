641ce9745e69386bd6691d61a700fa3f86c216c9
AboAbdelhameed <mohammad.abid.p95@gmail.com>
2020-12-16 09:50:47 +0200

    First Version

---
+++ server.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+import requests
+
+
+app = Flask(__name__, static_url_path='', 
+              static_folder='static', 
+              template_folder='templates'
+           )
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run( port = 3001 )
